a3b6535dd4994bada981112b1cf993e880743abd
KarlaBasurto <164158955+KarlaBasurto@users.noreply.github.com>
2026-05-14 00:31:42 +0200

    feat(shared): creación SideBar con un estilo inicial y con navegación activa

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.scss',
+  styleUrl: './sidebar.scss'
 })
-export class Sidebar {}
+export class SidebarComponent {
+  // Inyectamos el servicio para gestionar visibilidad por roles en el futuro
+  public authService = inject(AuthService);
+}
