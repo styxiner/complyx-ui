50d628c9bee9c3fa08ec2ff11521357ab2b417c9
KarlaBasurto <164158955+KarlaBasurto@users.noreply.github.com>
2026-05-14 00:30:08 +0200

    feat(layout):implementacion estructura base de AuthLayout y MainLayout

--- src/app/layout/auth-layout/auth-layout.ts
+++ src/app/layout/auth-layout/auth-layout.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
-  imports: [],
-  templateUrl: './auth-layout.html',
-  styleUrl: './auth-layout.scss',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <div class="auth-container">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styles: [`
+    .auth-container {
+      height: 100vh;
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #f0f2f5;
+    }
+  `]
 })
 export class AuthLayout {}
