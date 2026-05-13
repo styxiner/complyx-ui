d95c7e9d70e83682cf6f7204acd49b86e57c3fc1
KarlaBasurto <164158955+KarlaBasurto@users.noreply.github.com>
2026-05-13 20:17:13 +0200

    refactor(config): mover carpeta environments a la raiz de src

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, tap, map } from 'rxjs';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { API } from '../api.config';
 
 @Injectable({ providedIn: 'root' })
